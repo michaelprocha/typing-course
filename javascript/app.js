4b9c19ce46c914d4193d5bf72c3f4e0f0f5f5f61
michaelprocha <michaelrocha9722@gmail.com>
2025-09-10 14:00:02 -0300

    add separating text

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,11 @@
 (function () {
 	const levels = Array.from(document.querySelectorAll(".item-list"));
 	const texts = Array.from(document.querySelectorAll(".challenge"));
+	const textsChallenge = Array.from(document.querySelectorAll(".text-challenge"));
 	const input = document.getElementById("answer");
 	const timer = document.getElementById("timer");
 	let gameOver = true;
+	let changeLevel = false;
 
 	levels.forEach((level, i) => {
 		level.addEventListener("click", () => {
@@ -14,6 +16,7 @@
 					text.classList.remove("challenge-show");
 				}
 			});
+			changeLevel = true;
 		});
 	});
 
@@ -22,19 +25,46 @@
 		if (gameOver) {
 			gameOver = false;
 			let count = setInterval(() => {
-                console.log(gameOver);
 				runTimer--;
 				timer.textContent = `${runTimer}s`;
 				if (runTimer <= 0) {
-					clearInterval(count);
-					alert("Fim do tempo!");
 					gameOver = true;
+					alert("Fim do tempo!");
+					input.value = "";
+					clearInterval(count);
+				}
+				if (changeLevel == true) {
+					gameOver = true;
+					changeLevel = false;
+					input.value = "";
+					timer.textContent = `60s`;
+					clearInterval(count);
 				}
 			}, 1000);
 		}
 	}
 
+	function textSelect() {
+		if (gameOver == true) {
+			texts.forEach((element, i) => {
+				if (element.classList.contains("challenge-show")) {
+					let spanChar = "";
+					[...textsChallenge[i].textContent].forEach((char) => {
+						spanChar += `<span>${char}</span>`;
+					});
+					textsChallenge[i].innerHTML = spanChar;
+					textsChallenge[i].id = "current";
+					console.log(textsChallenge[i]);
+				}else{
+					textsChallenge[i].textContent = textsChallenge[i].textContent; 
+					textsChallenge[i].removeAttribute('id');
+				}
+			});
+		}
+	}
+
 	input.addEventListener("input", () => {
+		textSelect();
 		start();
 	});
 })();
