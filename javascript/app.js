54cca4e91910e8acb3ea2721791000ba8a84fa2f
michaelprocha <michaelrocha9722@gmail.com>
2025-09-10 19:06:07 -0300

    feat: typing course

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,12 @@
 	const textsChallenge = Array.from(document.querySelectorAll(".text-challenge"));
 	const input = document.getElementById("answer");
 	const timer = document.getElementById("timer");
+	const currentRight = document.getElementById("right-letter");
+	const currentWrong = document.getElementById("wrong-letter");
+	
+	let spanChar = "";
+	let right = 0;
+	let wrong = 0;
 	let gameOver = true;
 	let changeLevel = false;
 
@@ -17,6 +23,10 @@
 				}
 			});
 			changeLevel = true;
+			timer.textContent = '0';
+			currentRight.textContent = '0';
+			currentWrong.textContent = '0';
+			input.value = '';
 		});
 	});
 
@@ -26,7 +36,7 @@
 			gameOver = false;
 			let count = setInterval(() => {
 				runTimer--;
-				timer.textContent = `${runTimer}s`;
+				timer.textContent = `${runTimer}`;
 				if (runTimer <= 0) {
 					gameOver = true;
 					alert("Fim do tempo!");
@@ -37,7 +47,7 @@
 					gameOver = true;
 					changeLevel = false;
 					input.value = "";
-					timer.textContent = `60s`;
+					timer.textContent = `60`;
 					clearInterval(count);
 				}
 			}, 1000);
@@ -45,26 +55,62 @@
 	}
 
 	function textSelect() {
-		if (gameOver == true) {
+		if (gameOver) {
 			texts.forEach((element, i) => {
 				if (element.classList.contains("challenge-show")) {
-					let spanChar = "";
 					[...textsChallenge[i].textContent].forEach((char) => {
 						spanChar += `<span>${char}</span>`;
 					});
 					textsChallenge[i].innerHTML = spanChar;
 					textsChallenge[i].id = "current";
-					console.log(textsChallenge[i]);
-				}else{
-					textsChallenge[i].textContent = textsChallenge[i].textContent; 
-					textsChallenge[i].removeAttribute('id');
+				} else {
+					textsChallenge[i].textContent = textsChallenge[i].textContent;
+					textsChallenge[i].removeAttribute("id");
 				}
 			});
+		}
+	}
+
+	function game() {
+		if (!gameOver) {
+			const letter = [...document.querySelectorAll("#current span")];
+			letter.forEach((span, index) => {
+				if (timer.textContent != "0") {
+					if (input.value[index] == span.textContent) {
+						span.classList.add("right");
+						span.classList.remove("wrong");
+					} else if (input.value[index] == undefined) {
+						span.classList.remove("wrong");
+						span.classList.remove("right");
+					} else {
+						span.classList.add("wrong");
+						span.classList.remove("right");
+					}
+				} else {
+					span.classList.remove("wrong");
+					span.classList.remove("right");
+				}
+			});
+		}
+	}
+
+	function count() {
+		const letter = [...document.querySelectorAll("#current span")];
+		const letterRight = Array.from(document.getElementsByClassName("right"));
+		const letterWrong = Array.from(document.getElementsByClassName("wrong"));
+		right = letterRight.length;
+		wrong = letterWrong.length;
+		currentRight.textContent = right;
+		currentWrong.textContent = wrong;
+		if (right == letter.length) {
+			alert("Você conseguiu!");
 		}
 	}
 
 	input.addEventListener("input", () => {
 		textSelect();
 		start();
+		game();
+		count();
 	});
 })();
