46149accec94b83867a5473f866f31964d6323f3
michaelprocha <michaelrocha9722@gmail.com>
2025-09-11 11:50:43 -0300

    fix logic

--- javascript/app.js
+++ javascript/app.js
@@ -6,111 +6,141 @@
 	const timer = document.getElementById("timer");
 	const currentRight = document.getElementById("right-letter");
 	const currentWrong = document.getElementById("wrong-letter");
-	
+
 	let spanChar = "";
 	let right = 0;
 	let wrong = 0;
+	let runTimer = 60;
+	let win = false;
 	let gameOver = true;
-	let changeLevel = false;
+	let saveTexts = [];
+
+	textsChallenge.forEach((element, i) => {
+		saveTexts[i] = element.textContent.replace(/\s+/g, " ").trim();
+	});
+
+	console.log(textsChallenge);
+	console.log(saveTexts);
+
+	texts.forEach((element, i) => {
+		if (element.classList.contains("challenge-show")) {
+			[...textsChallenge[i].textContent].forEach((char) => {
+				spanChar += `<span>${char}</span>`;
+			});
+			textsChallenge[i].innerHTML = spanChar;
+			textsChallenge[i].id = "current";
+		}
+	});
 
 	levels.forEach((level, i) => {
 		level.addEventListener("click", () => {
-			texts.forEach((text, j) => {
-				if (i == j) {
-					text.classList.add("challenge-show");
-				} else {
-					text.classList.remove("challenge-show");
-				}
-			});
-			changeLevel = true;
-			timer.textContent = '0';
-			currentRight.textContent = '0';
-			currentWrong.textContent = '0';
-			input.value = '';
+			if (gameOver) {
+				texts.forEach((text, j) => {
+					if (i == j) {
+						text.classList.add("challenge-show");
+					} else {
+						text.classList.remove("challenge-show");
+					}
+				});
+				texts.forEach((element, i) => {
+					if (element.classList.contains("challenge-show")) {
+						[...textsChallenge[i].textContent].forEach((char) => {
+							spanChar += `<span>${char}</span>`;
+						});
+						textsChallenge[i].innerHTML = spanChar;
+						textsChallenge[i].id = "current";
+						spanChar = "";
+					} else {
+						textsChallenge[i].textContent = saveTexts[i];
+						textsChallenge[i].removeAttribute("id");
+					}
+				});
+			}
 		});
 	});
 
-	function start() {
-		let runTimer = 60;
-		if (gameOver) {
-			gameOver = false;
-			let count = setInterval(() => {
-				runTimer--;
-				timer.textContent = `${runTimer}`;
-				if (runTimer <= 0) {
-					gameOver = true;
-					alert("Fim do tempo!");
-					input.value = "";
-					clearInterval(count);
-				}
-				if (changeLevel == true) {
-					gameOver = true;
-					changeLevel = false;
-					input.value = "";
-					timer.textContent = `60`;
-					clearInterval(count);
-				}
-			}, 1000);
-		}
-	}
-
-	function textSelect() {
-		if (gameOver) {
-			texts.forEach((element, i) => {
-				if (element.classList.contains("challenge-show")) {
-					[...textsChallenge[i].textContent].forEach((char) => {
-						spanChar += `<span>${char}</span>`;
-					});
-					textsChallenge[i].innerHTML = spanChar;
-					textsChallenge[i].id = "current";
-				} else {
-					textsChallenge[i].textContent = textsChallenge[i].textContent;
-					textsChallenge[i].removeAttribute("id");
-				}
-			});
-		}
-	}
-
-	function game() {
-		if (!gameOver) {
-			const letter = [...document.querySelectorAll("#current span")];
-			letter.forEach((span, index) => {
-				if (timer.textContent != "0") {
-					if (input.value[index] == span.textContent) {
-						span.classList.add("right");
-						span.classList.remove("wrong");
-					} else if (input.value[index] == undefined) {
-						span.classList.remove("wrong");
-						span.classList.remove("right");
-					} else {
-						span.classList.add("wrong");
-						span.classList.remove("right");
-					}
-				} else {
+	function gameProgress() {
+		const letter = [...document.querySelectorAll("#current span")];
+		letter.forEach((span, index) => {
+			if (gameOver == false) {
+				if (input.value[index] == span.textContent) {
+					span.classList.add("right");
+					span.classList.remove("wrong");
+				} else if (input.value[index] == undefined) {
 					span.classList.remove("wrong");
 					span.classList.remove("right");
+				} else {
+					span.classList.add("wrong");
+					span.classList.remove("right");
 				}
-			});
-		}
+			}
+		});
 	}
 
 	function count() {
-		const letter = [...document.querySelectorAll("#current span")];
 		const letterRight = Array.from(document.getElementsByClassName("right"));
 		const letterWrong = Array.from(document.getElementsByClassName("wrong"));
 		right = letterRight.length;
 		wrong = letterWrong.length;
 		currentRight.textContent = right;
 		currentWrong.textContent = wrong;
-		if (right == letter.length) {
-			alert("Você conseguiu!");
+	}
+
+	function game() {
+		if (gameOver) {
+			gameOver = false;
+			let intervalId = setInterval(() => {
+				runTimer--;
+				timer.textContent = `${runTimer}`;
+				if (runTimer <= 0) {
+					gameOver = true;
+					input.value = "";
+					currentRight.textContent = "0";
+					currentWrong.textContent = "0";
+					timer.textContent = "60";
+					runTimer = 60;
+					alert("Fim do tempo!");
+					clearInterval(intervalId);
+				}
+				if (win) {
+					win = false;
+					gameOver = true;
+					input.value = "";
+					currentRight.textContent = "0";
+					currentWrong.textContent = "0";
+					timer.textContent = `60`;
+					runTimer = 60;
+					alert("Você conseguiu!");
+					clearInterval(intervalId);
+				}
+			}, 1000);
+		}
+	}
+
+	function endGame() {
+		const letter = [...document.querySelectorAll("#current span")];
+		if (gameOver) {
+			letter.forEach((span) => {
+				span.classList.remove("wrong");
+				span.classList.remove("right");
+			});
+		}
+	}
+
+	function winGame() {
+		const letter = [...document.querySelectorAll("#current span")];
+		const letterRight = Array.from(document.getElementsByClassName("right"));
+		if (letter.length == letterRight.length) {
+			win = true;
 		}
 	}
 
 	input.addEventListener("input", () => {
-		textSelect();
-		start();
 		game();
+		gameProgress();
 		count();
+		game();
+		winGame();
+		endGame();
 	});
 })();
